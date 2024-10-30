747c64ea90c9806d19c1e3e00c4794c663f81863
mariaisabelocampo <98433396+mariaisabelocampo@users.noreply.github.com>
2024-10-30 15:23:42 -0500

    Add files via upload

---
+++ AuthService.cs
@@ -0,0 +1,27 @@
+﻿// Services/AuthService.cs
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+
+namespace Login.Services
+{
+    public class AuthService
+    {
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public AuthService(SignInManager<IdentityUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
+        public async Task<bool> LoginAsync(string email, string password)
+        {
+            var result = await _signInManager.PasswordSignInAsync(email, password, isPersistent: false, lockoutOnFailure: false);
+            return result.Succeeded;
+        }
+
+        public async Task LogoutAsync()
+        {
+            await _signInManager.SignOutAsync();
+        }
+    }
+}
